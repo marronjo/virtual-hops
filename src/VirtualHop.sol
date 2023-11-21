8bbd61b0938d68cae4d353ca7c50eb68b94141c4
Joe Marron <marronjo@tcd.ie>
2023-11-21 22:35:02 +0000

    add contract skeleton

---
+++ src/VirtualHop.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+contract VirtualHop {
+
+    /**
+     * send message / tokens from source chain
+     */
+    function send() external {
+
+    }
+    
+    /**
+     * receive message / tokens from source chain on 
+     * intermediary chain send onto destination chain
+     */
+    function hop() external {
+
+    }
+
+    /**
+     * receive message / tokens on destination chain
+     */
+    function collect() external {
+
+    }
+}
