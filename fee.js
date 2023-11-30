d0c5e5d553d2798e3706a3e43b29e830d19d866f
Joe Marron <marronjo@tcd.ie>
2023-11-30 16:46:58 +0000

    improve chain config map

--- fee.js
+++ fee.js
@@ -4,61 +4,35 @@ require('dotenv').config();
 
 const chainConfigs = [
     {
-        "name": "ETH",
+        "network": "ETH",
         "deployedContract": "0xc2575DFc9a9487E3d5a58288A292d1f068A4e5bb",
         "chainSelector": "16015286601757825753",
         "rpcEndpoint": process.env.API_KEY_ETH
     },
     {
-        "name": "OP",
+        "network": "OP",
         "deployedContract": "0x45776686c138e782Fb9ea26FFd54A6C3EAAbf677",
         "chainSelector": "2664363617261496610",
         "rpcEndpoint": process.env.API_KEY_OPTIMISM
     },
     {
-        "name": "POL",
+        "network": "POL",
         "deployedContract": "0xA52CB4d41bB098BaC3011664D6a4aA740057948f",
         "chainSelector": "12532609583862916517",
         "rpcEndpoint": process.env.API_KEY_POLYGON
     },
     {
-        "name": "AVAX",
+        "network": "AVAX",
         "deployedContract": "0xadD2970aAbA4814E572f39c041b3DE04DC9278c3",
         "chainSelector": "14767482510784806043",
         "rpcEndpoint": process.env.API_KEY_AVAX
     },
 ]
 
-//excluding ETH Sepolia
-//if sepolia chain selector is sent to sepolia router, it will throw error!
-const chainSelectors = [
-    '2664363617261496610',   
-    '12532609583862916517',
-    '14767482510784806043',
-    '13264668187771770619',
-    '5790810961207155433'
-]
-
 const destinationAddress = '0xb592b6313f005Ade818FcdE0f64bc42AB23eD700';    //random address to send tokens to
 const amount = Math.pow(10,18).toString();                                  //1ether 10**18
 
-async function main(){
-    var provider = new ethers.providers.JsonRpcProvider(process.env.API_KEY_ETH);
-
-    const feeContractSepolia = new ethers.Contract(
-        '0xc2575DFc9a9487E3d5a58288A292d1f068A4e5bb',
-        Fee.abi,
-        provider
-    );
-
-    for(const chainSelector of chainSelectors) {
-        let fee = await feeContractSepolia.getFee(chainSelector, amount, destinationAddress);
-        let etherValue = ethers.utils.formatEther(fee);
-        console.log(etherValue);
-    }
-}
-
-async function getAllFees(){
+function getAllFees(){
     let allFees = []
     for(const chainConfig of chainConfigs){
         var provider = new ethers.providers.JsonRpcProvider(chainConfig.rpcEndpoint);
@@ -69,9 +43,10 @@ async function getAllFees(){
             provider
         );
 
-        let fees = getFee(feeContract, chainConfig.name);
+        let fees = getFee(feeContract, chainConfig.network);
         allFees.push(fees);
     }
+    return allFees;
 }
 
 async function getFee(contract, network){
@@ -79,7 +54,7 @@ async function getFee(contract, network){
     for(const chainConfig of getOtherChainConfigs(network)) {
         let fee = await contract.getFee(chainConfig.chainSelector, amount, destinationAddress);
         let etherValue = ethers.utils.formatEther(fee);
-        console.log('%s\t ->\t %s\t : %f', network, chainConfig.name, etherValue);
+        console.log('%s\t ->\t %s\t : %f', network, chainConfig.network, etherValue);
         fees.push(etherValue);
     }
     return fees;
@@ -89,13 +64,11 @@ async function getFee(contract, network){
 function getOtherChainConfigs(network){
     let configs = []
     for(const chainConfig of chainConfigs){
-        if(network != chainConfig.name){
+        if(network != chainConfig.network){
             configs.push(chainConfig);
         }
     }
     return configs;
 }
-
-//main();
 
 getAllFees();
