981166aa5fc5e8c668d55d05e2b98dd92fe0f3c2
Joe Marron <marronjo@tcd.ie>
2023-11-30 13:47:36 +0000

    add chain config to fee script

--- fee.js
+++ fee.js
@@ -2,6 +2,33 @@ const ethers = require('ethers');
 const Fee = require('./out/Fee.sol/Fee.json');
 require('dotenv').config();
 
+const chainConfig = [
+    {
+        "name": "ETH",
+        "deployedContract": "0xc2575DFc9a9487E3d5a58288A292d1f068A4e5bb",
+        "chainSelector": "16015286601757825753",
+        "rpcEndpoint": process.env.API_KEY_ETH
+    },
+    {
+        "name": "OP",
+        "deployedContract": "0x45776686c138e782Fb9ea26FFd54A6C3EAAbf677",
+        "chainSelector": "2664363617261496610",
+        "rpcEndpoint": process.env.API_KEY_OPTIMISM
+    },
+    {
+        "name": "POL",
+        "deployedContract": "0xA52CB4d41bB098BaC3011664D6a4aA740057948f",
+        "chainSelector": "12532609583862916517",
+        "rpcEndpoint": process.env.API_KEY_POLYGON
+    },
+    {
+        "name": "AVAX",
+        "deployedContract": "0xadD2970aAbA4814E572f39c041b3DE04DC9278c3",
+        "chainSelector": "14767482510784806043",
+        "rpcEndpoint": process.env.API_KEY_AVAX
+    },
+]
+
 //excluding ETH Sepolia
 const chainSelectors = [
     '2664363617261496610',   
